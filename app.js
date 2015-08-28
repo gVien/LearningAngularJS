374a0fdb42fc50bbd8f0bf3de78997245d3b0dd9
Gai Vien <email": null>
2015-08-27 20:11:39 -0700

    create button to add post and generalize post function

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ angular.module("flapperNews", [])
 function($scope) {
   $scope.test = "Hello World!";
 
-// displaying Lists
-// $scope variable serves as the bridge between Angular controllers
-// and Angular templates
-// if we want something to be accessible in the template (fxn or variable) then bind it to $scope
-// ng-repeat has to be used in index.html
-$scope.posts = [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 21},
-  {title: 'post 4', upvotes: 23},
-  {title: 'post 5', upvotes: 27}
-];
+  // displaying Lists
+  // $scope variable serves as the bridge between Angular controllers
+  // and Angular templates
+  // if we want something to be accessible in the template (fxn or variable) then bind it to $scope
+  // ng-repeat has to be used in index.html
+  $scope.posts = [
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 21},
+    {title: 'post 4', upvotes: 23},
+    {title: 'post 5', upvotes: 27}
+  ];
+
+  // scope is the object that refers to the application model
+  $scope.addPost = function() {
+    if (!$scope.title || $scope.title === "") {return;};
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = "";
+  };
 }]);
