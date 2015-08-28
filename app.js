01e68d56b4255c46b8d02c31dcb8eea5343d9f0a
Gai Vien <email": null>
2015-08-27 19:45:27 -0700

    add posts

--- app.js
+++ app.js
@@ -3,4 +3,17 @@ angular.module("flapperNews", [])
 "$scope",
 function($scope) {
   $scope.test = "Hello World!";
+
+// displaying Lists
+// $scope variable serves as the bridge between Angular controllers
+// and Angular templates
+// if we want something to be accessible in the template (fxn or variable) then bind it to $scope
+// ng-repeat has to be used in index.html
+$scope.posts = [
+  'post 1',
+  'post 2',
+  'post 3',
+  'post 4',
+  'post 5'
+];
 }]);
